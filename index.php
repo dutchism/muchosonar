1ce729e726d65e0c81a80d9e1b18cf980e8812b1
dutchism <dutchism@users.noreply.github.com>
2020-02-21 13:10:03 +1100

    Add Home Page

---
+++ index.php
@@ -0,0 +1,231 @@
+<?php include("header.php"); ?>
+
+			
+			<div class="section sounds">
+				
+			<img src="images/MuchoSonarHeaderRed.jpg">
+
+			<!--<h4>Latest Release - Know Your Product (The Saints)</h4>
+			<audio id="myAudio" controls>				  
+				  <source src="audio/Devil's Call.mp3" type="audio/mpeg">
+				  Your browser does not support the audio element.
+				</audio>
+				<script>
+					var x = document.getElementById("myAudio");
+					x.volume = 0.8;
+				</script>
+				<a class="btn" href="audio/Devil's Call.mp3" download="Devil's Call.mp3">Download</a>
+
+				<hr>-->
+
+				<div class="image">
+					<svg version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
+	  viewBox="0 0 24 24" enable-background="new 0 0 24 24" xml:space="preserve">
+						<g id="Free_Icons">
+							<g>
+								<polygon class="icon-line" fill="none" stroke="#000000" stroke-linejoin="round" stroke-miterlimit="10" points="6.5,12.5 6.5,23.5 3.5,19.5 
+									3.5,16.5 		"/>
+								<polyline class="icon-line" fill="none" stroke="#000000" stroke-linejoin="round" stroke-miterlimit="10" points="4.5,5 0.5,7 3,13 3,16.5 		"/>
+								<polyline class="icon-line" fill="none" stroke="#000000" stroke-linejoin="round" stroke-miterlimit="10" points="3.5,16.5 1.5,16.5 1.5,19.5 
+									3.5,19.5 		"/>
+								<polyline class="icon-line" fill="none" stroke="#000000" stroke-linejoin="round" stroke-miterlimit="10" points="6.5,12.5 8.5,12.5 8.5,23.5 
+									6.5,23.5 		"/>
+								<polygon class="icon-line" fill="none" stroke="#000000" stroke-linejoin="round" stroke-miterlimit="10" points="17.5,12.5 17.5,23.5 20.5,19.5 
+									20.5,16.5 		"/>
+								<polyline class="icon-line" fill="none" stroke="#000000" stroke-linejoin="round" stroke-miterlimit="10" points="19.5,5 23.5,7 21,13 21,16.5 		
+									"/>
+								<polyline class="icon-line" fill="none" stroke="#000000" stroke-linejoin="round" stroke-miterlimit="10" points="20.5,16.5 22.5,16.5 22.5,19.5 
+									20.5,19.5 		"/>
+								<polyline class="icon-line" fill="none" stroke="#000000" stroke-linejoin="round" stroke-miterlimit="10" points="17.5,12.5 15.5,12.5 15.5,23.5 
+									17.5,23.5 		"/>
+								<polygon class="icon-line" fill="none" stroke="#000000" stroke-linejoin="round" stroke-miterlimit="10" points="12,3 19,6 20,4 12,0.5 4,4 5,6 		
+									"/>
+							</g>
+						</g>
+					</svg>
+				</div>
+				<h2>Sonidos</h2>
+				<h4>"Murdochracy" - EP</h4>
+				<div class="iframewrapper">
+				<iframe style="border: 0; height: 1004px;" src="https://bandcamp.com/EmbeddedPlayer/album=3560965929/size=large/bgcol=333333/linkcol=e32c14/transparent=true/" seamless><a href="http://muchosonar.bandcamp.com/album/murdochracy">Murdochracy by Mucho Sonar</a></iframe>
+				</div>
+				<a class="btn" href="mucho-sonar-listen.php">Give me more</a>
+				<p>&nbsp;</p>
+				
+				<svg id="download-icon" version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" viewBox="0 0 24 24" enable-background="new 0 0 24 24" xml:space="preserve">
+					<g>
+						<g>
+							<polyline class="icon-line" fill="none" stroke="#000000" stroke-linecap="round" stroke-linejoin="round" stroke-miterlimit="10" points="5.5,15.5 
+								3.5,15.5 0.5,12.5 0.5,8.5 3.5,5.5 6.5,5.5 6.5,3.5 9.5,0.5 14.5,0.5 17.5,3.5 17.5,5.5 21,5.5 23.5,8 23.5,12.5 20.5,15.5 
+								18.5,15.5 		"/>
+							
+								<line class="icon-line" fill="none" stroke="#000000" stroke-linecap="round" stroke-linejoin="round" stroke-miterlimit="10" x1="11.5" y1="10.5" x2="11.5" y2="23.5"/>
+							<polyline class="icon-line" fill="none" stroke="#000000" stroke-linecap="round" stroke-linejoin="round" stroke-miterlimit="10" points="7.5,19.5 
+								11.5,23.5 15.5,19.5 		"/>
+						</g>
+					</g>
+				</svg>
+				<div class="social">
+					<ul>						
+						<li id="bandcamp"><a href="https://muchosonar.bandcamp.com/" target="_blank"><svg version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" viewBox="0 0 46 46" enable-background="new 0 0 46 46" xml:space="preserve">
+							<polygon class="icon-line" fill="none" stroke-width="2" stroke-linecap="square" stroke-linejoin="round" stroke-miterlimit="10" points="
+									10.531,43 2.819,18.082 23.01,2.822 43.199,18.377 35.487,43 	"/>
+							<path class="icon-fill" d="M22.39,17.993v4.057h0.03c0.213-0.436,0.547-0.751,1.002-0.951c0.456-0.196,0.958-0.295,1.504-0.295
+										c0.608,0,1.138,0.112,1.588,0.333c0.451,0.224,0.826,0.524,1.124,0.905c0.299,0.38,0.525,0.817,0.677,1.314
+										s0.229,1.023,0.229,1.581s-0.074,1.085-0.221,1.581c-0.147,0.495-0.371,0.928-0.669,1.298c-0.3,0.371-0.673,0.661-1.124,0.874
+										c-0.45,0.214-0.976,0.319-1.572,0.319c-0.193,0-0.408-0.02-0.646-0.061c-0.239-0.042-0.475-0.106-0.708-0.197
+										c-0.232-0.091-0.453-0.216-0.66-0.372c-0.208-0.158-0.383-0.353-0.525-0.585h-0.03v1.048h-1.291v-10.85H22.39z M27.032,23.775
+										c-0.097-0.349-0.243-0.661-0.441-0.934c-0.197-0.274-0.451-0.491-0.759-0.653c-0.311-0.162-0.672-0.244-1.087-0.244
+										c-0.436,0-0.805,0.087-1.109,0.259c-0.304,0.171-0.553,0.397-0.745,0.676c-0.192,0.279-0.332,0.596-0.417,0.949
+										c-0.086,0.355-0.129,0.714-0.129,1.079c0,0.386,0.046,0.758,0.137,1.118c0.091,0.359,0.235,0.676,0.433,0.948
+										c0.198,0.274,0.454,0.496,0.767,0.662c0.314,0.167,0.694,0.25,1.141,0.25c0.445,0,0.818-0.085,1.117-0.257
+										c0.298-0.173,0.539-0.399,0.721-0.685c0.184-0.284,0.314-0.607,0.396-0.973c0.081-0.364,0.121-0.74,0.121-1.125
+										C27.176,24.481,27.129,24.125,27.032,23.775z"/>
+									<path class="icon-fill" d="M33.998,22.354c-0.323-0.273-0.76-0.411-1.306-0.411c-0.467,0-0.858,0.087-1.171,0.259
+										c-0.314,0.171-0.565,0.399-0.752,0.684c-0.188,0.283-0.322,0.61-0.403,0.979c-0.079,0.37-0.12,0.752-0.12,1.147
+										c0,0.364,0.042,0.72,0.128,1.064c0.086,0.344,0.221,0.651,0.401,0.918c0.185,0.268,0.422,0.484,0.716,0.646
+										c0.293,0.162,0.643,0.243,1.049,0.243c0.638,0,1.137-0.167,1.497-0.5c0.359-0.335,0.579-0.806,0.659-1.414h1.323
+										c-0.143,0.973-0.504,1.722-1.086,2.25c-0.583,0.525-1.376,0.789-2.379,0.789c-0.598,0-1.127-0.096-1.587-0.288
+										c-0.462-0.192-0.847-0.467-1.154-0.82c-0.311-0.355-0.543-0.778-0.7-1.27c-0.156-0.49-0.236-1.03-0.236-1.618
+										s0.076-1.138,0.229-1.648c0.151-0.513,0.382-0.958,0.691-1.338c0.308-0.38,0.695-0.679,1.162-0.897
+										c0.466-0.217,1.008-0.326,1.626-0.326c0.445,0,0.864,0.053,1.254,0.159c0.39,0.107,0.732,0.271,1.033,0.495
+										c0.298,0.223,0.544,0.504,0.736,0.843c0.192,0.34,0.314,0.743,0.365,1.209h-1.338C34.535,23.014,34.322,22.628,33.998,22.354z"/>
+								<polygon class="icon-fill" points="8.253,29 17.748,29 21.45,18 11.955,18 	"/>
+							</svg>
+							</a>
+						</li>
+						<li id="itunes"><a href="https://itunes.apple.com/au/artist/mucho-sonar/id813505702" target="_blank"><svg version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" viewBox="0 0 46 46" enable-background="new 0 0 46 46" xml:space="preserve">
+							<polygon class="icon-line" fill="none" stroke-width="2" stroke-linecap="square" stroke-linejoin="round" stroke-miterlimit="10" points="
+									10.531,43 2.819,18.082 23.01,2.822 43.199,18.377 35.487,43 	"/>
+								<path class="icon-fill" d="M27.916,25.07c-0.024-2.281,1.859-3.383,1.948-3.433c-1.059-1.546-2.712-1.761-3.299-1.786
+									c-1.399-0.141-2.735,0.829-3.442,0.829c-0.717,0-1.816-0.809-2.979-0.786c-1.526,0.023-2.932,0.889-3.72,2.259
+									c-1.589,2.749-0.404,6.833,1.135,9.07c0.756,1.088,1.666,2.323,2.852,2.274c1.133-0.039,1.564-0.737,2.944-0.737
+									c1.379,0,1.77,0.737,2.977,0.719c1.217-0.021,2.002-1.112,2.75-2.213c0.867-1.266,1.229-2.499,1.248-2.558
+									C30.304,28.696,27.943,27.786,27.916,25.07 M25.646,18.373c0.627-0.762,1.049-1.819,0.934-2.872
+									c-0.897,0.029-1.996,0.598-2.644,1.36c-0.584,0.679-1.102,1.754-0.958,2.786C23.987,19.725,25.022,19.135,25.646,18.373"/>
+							</svg>
+							</a>
+						</li>
+						<li id="google"><a href="https://play.google.com/store/music/artist/Mucho_Sonar?id=A3rtkzoigw4bxlnyordycxbbana&hl=en" target="_blank"><svg version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" viewBox="0 0 46 46" enable-background="new 0 0 46 46" xml:space="preserve">
+							<polygon class="icon-line" fill="none" stroke-width="2" stroke-linecap="square" stroke-linejoin="round" stroke-miterlimit="10" points="
+									10.531,43 2.819,18.082 23.01,2.822 43.199,18.377 35.487,43 	"/>
+							<g id="Layer_1">
+								<g>
+									<polygon class="icon-line" fill="none" stroke-width="2" stroke-linejoin="round" stroke-miterlimit="10" points="15,16 15,34 
+										31.875,25.364 		"/>
+									
+										<line class="icon-line" fill="none" stroke-width="1.5" stroke-linejoin="round" stroke-miterlimit="10" x1="15.341" y1="16.5" x2="27.986" y2="27.229"/>
+									
+										<line class="icon-line" fill="none" stroke-width="1.5" stroke-linejoin="round" stroke-miterlimit="10" x1="15.5" y1="34" x2="27.954" y2="22.911"/>
+								</g>
+							</g>
+							</svg>
+							</a>
+						</li>
+						<li id="amazon"><a href="https://www.amazon.com/Rodents-Mucho-Sonar/dp/B01HATSWGW/ref=sr_1_2?s=dmusic&ie=UTF8&qid=1478045745&sr=1-2-mp3-albums-bar-strip-0&keywords=Mucho+Sonar" target="_blank"><svg version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" viewBox="0 0 46 46" enable-background="new 0 0 46 46" xml:space="preserve">
+								<polygon class="icon-line" fill="none" stroke-width="2" stroke-linecap="square" stroke-linejoin="round" stroke-miterlimit="10" points="
+									10.531,43 2.819,18.082 23.01,2.822 43.199,18.377 35.487,43 	"/>
+							<g id="Layer_1">
+								<g>
+									<path class="icon-fill" fill-rule="evenodd" clip-rule="evenodd" d="M29.394,30.192c-0.089,0-0.183,0.021-0.267,0.062
+										c-0.098,0.039-0.196,0.082-0.289,0.123l-0.137,0.056l-0.178,0.071v0.002c-1.931,0.783-3.96,1.242-5.837,1.283
+										c-0.069,0.002-0.138,0.002-0.206,0.002c-2.951,0.002-5.36-1.369-7.792-2.718c-0.085-0.045-0.17-0.067-0.256-0.067
+										c-0.109,0-0.222,0.041-0.303,0.117C14.048,29.199,14,29.312,14,29.424c0,0.148,0.08,0.283,0.189,0.371
+										c2.281,1.98,4.781,3.82,8.143,3.821c0.065,0,0.133-0.001,0.199-0.003c2.139-0.048,4.559-0.77,6.436-1.949l0.01-0.008
+										c0.247-0.146,0.493-0.314,0.725-0.499c0.145-0.106,0.244-0.274,0.241-0.446C29.938,30.401,29.676,30.192,29.394,30.192"/>
+									<path class="icon-fill" fill-rule="evenodd" clip-rule="evenodd" d="M31.998,29.1v-0.002c-0.008-0.188-0.047-0.33-0.125-0.448
+										l-0.008-0.011l-0.011-0.013c-0.079-0.087-0.155-0.12-0.237-0.155c-0.246-0.096-0.605-0.146-1.037-0.147
+										c-0.311,0-0.653,0.028-0.998,0.104v-0.022l-0.347,0.114l-0.006,0.004l-0.194,0.062v0.01c-0.229,0.094-0.438,0.213-0.633,0.354
+										c-0.119,0.091-0.221,0.21-0.227,0.393c-0.002,0.1,0.048,0.215,0.133,0.282c0.083,0.068,0.18,0.091,0.266,0.091
+										c0.021,0,0.039,0,0.057-0.004l0.017-0.002l0.013-0.002c0.17-0.036,0.416-0.061,0.705-0.102c0.248-0.026,0.51-0.047,0.737-0.047
+										c0.162,0,0.306,0.011,0.405,0.033c0.05,0.01,0.088,0.022,0.109,0.032c0.006,0.003,0.01,0.006,0.014,0.009
+										c0.005,0.014,0.011,0.049,0.01,0.1c0.003,0.189-0.08,0.544-0.189,0.888c-0.107,0.345-0.24,0.69-0.326,0.921
+										c-0.021,0.055-0.034,0.111-0.034,0.176c-0.003,0.092,0.036,0.203,0.116,0.278c0.078,0.074,0.179,0.104,0.265,0.104h0.003
+										c0.127-0.002,0.234-0.054,0.329-0.125c0.879-0.791,1.185-2.053,1.196-2.763L31.998,29.1z"/>
+									<path class="icon-fill" fill-rule="evenodd" clip-rule="evenodd" d="M23.964,20.63c-0.517,0.039-1.112,0.079-1.709,0.157
+										c-0.914,0.121-1.827,0.278-2.582,0.637c-1.469,0.596-2.462,1.866-2.462,3.733c0,2.345,1.509,3.534,3.416,3.534
+										c0.635,0,1.152-0.079,1.628-0.197c0.755-0.239,1.39-0.676,2.146-1.471c0.437,0.597,0.557,0.875,1.311,1.51
+										c0.199,0.079,0.398,0.079,0.557-0.039c0.477-0.398,1.312-1.113,1.747-1.51c0.199-0.159,0.159-0.398,0.039-0.594
+										c-0.437-0.558-0.873-1.035-0.873-2.107v-3.574c0-1.51,0.119-2.901-0.993-3.932c-0.912-0.834-2.344-1.152-3.455-1.152h-0.478
+										c-2.024,0.118-4.17,0.992-4.646,3.495c-0.08,0.319,0.159,0.437,0.317,0.476l2.225,0.278c0.238-0.039,0.356-0.239,0.397-0.435
+										c0.198-0.875,0.913-1.312,1.707-1.392h0.16c0.476,0,0.991,0.199,1.271,0.597c0.317,0.476,0.278,1.112,0.278,1.668V20.63z
+										 M23.527,25.355c-0.278,0.556-0.756,0.914-1.272,1.035c-0.079,0-0.198,0.039-0.317,0.039c-0.873,0-1.39-0.677-1.39-1.67
+										c0-1.271,0.754-1.867,1.707-2.145c0.517-0.119,1.112-0.159,1.709-0.159v0.478C23.964,23.846,24.003,24.561,23.527,25.355"/>
+								</g>
+							</g>
+							</svg>
+							</a>
+						</li>
+						<li id="spotify"><a href="https://play.spotify.com/artist/5LCVHxA4wuE0IRp0EbZr8q?play=true&utm_source=open.spotify.com&utm_medium=open" target="_blank"><svg version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" viewBox="0 0 46 46" enable-background="new 0 0 46 46" xml:space="preserve">
+							<polygon class="icon-line" fill="none" stroke-width="2" stroke-linecap="square" stroke-linejoin="round" stroke-miterlimit="10" points="
+									10.531,43 2.819,18.082 23.01,2.822 43.199,18.377 35.487,43 	"/>
+							<path class="icon-fill" d="M22.995,15.251c-5.382,0-9.739,4.366-9.739,9.75c0,2.467,0.904,4.714,2.413,6.428
+									c1.528-1.519,3.616-2.384,6.01-2.311c3.059,0.1,5.477,1.554,6.861,3.904c2.546-1.754,4.205-4.693,4.205-8.021
+									C32.745,19.617,28.387,15.251,22.995,15.251 M27.763,28.479c-0.163,0.283-0.531,0.375-0.822,0.199
+									c-2.249-1.38-5.093-1.687-8.437-0.923c-0.324,0.065-0.652-0.129-0.716-0.456c-0.074-0.318,0.129-0.643,0.447-0.716
+									c3.664-0.835,6.801-0.479,9.337,1.069C27.85,27.821,27.946,28.196,27.763,28.479 M28.942,25.72
+									c-0.216,0.352-0.672,0.453-1.025,0.239c-2.587-1.586-6.523-2.043-9.583-1.122c-0.396,0.126-0.818-0.101-0.938-0.499
+									c-0.12-0.396,0.105-0.812,0.506-0.938c3.49-1.051,7.829-0.537,10.792,1.278C29.045,24.899,29.156,25.36,28.942,25.72 M29.04,22.848
+									c-3.085-1.84-8.198-2.011-11.163-1.116c-0.475,0.152-0.972-0.123-1.116-0.598c-0.145-0.47,0.125-0.974,0.597-1.121
+									c3.39-1.03,9.042-0.829,12.601,1.291c0.435,0.252,0.564,0.803,0.322,1.226C30.027,22.959,29.474,23.101,29.04,22.848
+									 M21.618,32.037c-1.369-0.037-2.513,0.494-3.34,1.489c1.398,0.782,3.01,1.223,4.716,1.223c0.833,0,1.637-0.105,2.404-0.296
+									C24.667,32.942,23.383,32.1,21.618,32.037"/>
+							</svg>
+							</a>
+						</li>
+					</ul>
+				</div>
+			</div>
+			<div class="section bio">
+				<div class="image">
+					<svg version="1.0" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" viewBox="0 0 24 24" style="enable-background:new 0 0 24 24;" xml:space="preserve">
+					<g>
+						<g>
+							<line class="icon-line" style="fill:none;stroke-linecap:round;stroke-linejoin:round;stroke-miterlimit:10;" x1="21.5" y1="0.5" x2="21.5" y2="2"/>
+							<polygon class="icon-line" style="fill:none;stroke-linecap:round;stroke-linejoin:round;stroke-miterlimit:10;" points="21.5,2 
+								19.5,5.5 21.5,7.5 23.5,5.5 		"/>
+							<polyline class="icon-line" style="fill:none;stroke-linecap:round;stroke-linejoin:round;stroke-miterlimit:10;" points="19.5,5.5 
+								19.5,21.5 23.5,21.5 23.5,6 		"/>
+							
+								<line class="icon-line" style="fill:none;stroke-linecap:round;stroke-linejoin:round;stroke-miterlimit:10;" x1="21.5" y1="7.5" x2="21.5" y2="19.5"/>
+							<polyline class="icon-line" style="fill:none;stroke-linecap:round;stroke-linejoin:round;stroke-miterlimit:10;" points="19.5,21.5 
+								20.5,23.5 22.5,23.5 23.5,21.5 		"/>
+							
+								<rect class="icon-line" x="0.5" y="2.5" style="fill:none;stroke-linecap:round;stroke-linejoin:round;stroke-miterlimit:10;" width="15" height="21"/>
+							<polygon class="icon-line" style="fill:none;stroke-linejoin:round;stroke-miterlimit:10;" points="11.5,11 8,12.5 4.5,11 4.5,7 
+								8,5.5 11.5,7 		"/>
+							<line class="icon-line" style="fill:none;stroke-linejoin:round;stroke-miterlimit:10;" x1="0.5" y1="16.5" x2="15.5" y2="16.5"/>
+							<line class="icon-line" style="fill:none;;stroke-linejoin:round;stroke-miterlimit:10;" x1="0.5" y1="19.5" x2="15.5" y2="19.5"/>
+							<line class="icon-line" style="fill:none;stroke-linecap:round;stroke-linejoin:round;stroke-miterlimit:10;" x1="19.5" y1="19.5" x2="23.5" y2="19.5"/>
+						</g>
+					</g>
+					</svg>
+				</div>
+				<h2>Biografía</h2>
+				<blockquote>“Their sound is best described as Motorhead plays Little Richard, complete with face melting horn section”</blockquote>
+				<a class="btn" href="about-mucho-sonar.php">Read on</a>
+			</div>
+			<div class="section gallery">
+				<div class="image">
+					<svg version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" viewBox="0 0 24 24" enable-background="new 0 0 24 24" xml:space="preserve">
+						<g id="Free_Icons">
+							<g>
+									<rect class="icon-line" x="0.5" y="8.5" fill="none" stroke="#000000" stroke-linecap="round" stroke-linejoin="round" stroke-miterlimit="10" width="23" height="15"/>
+								
+									<line class="icon-line" fill="none" stroke="#000000" stroke-linecap="round" stroke-linejoin="round" stroke-miterlimit="10" x1="8.5" y1="8.5" x2="8.5" y2="23.5"/>
+								
+									<rect class="icon-line" x="1.5" y="5.5" fill="none" stroke="#000000" stroke-linecap="round" stroke-linejoin="round" stroke-miterlimit="10" width="6" height="3"/>
+								<polyline class="icon-line" fill="none" stroke="#000000" stroke-linecap="round" stroke-linejoin="round" stroke-miterlimit="10" points="
+									16.5,13.5 15,13.5 13.5,15 13.5,16.5 		"/>
+								<polygon class="icon-line" fill="none" stroke="#000000" stroke-linejoin="round" stroke-miterlimit="10" points="18,20.5 14,20.5 11.5,18 11.5,14 
+									14,11.5 18,11.5 20.5,14 20.5,18 		"/>
+							</g>
+						</g>
+					</svg>
+				</div>
+				<h2>Galeria</h2>
+				<img class="hero" src="images/MuchoStage.jpg" alt="Mucho Sonar in your eyes" title="Mucho Sonar in your eyes">
+				<a class="btn" href="mucho-sonar-gallery.php">Check it</a>
+			</div>
+
+<?php include("footer.php"); ?>
